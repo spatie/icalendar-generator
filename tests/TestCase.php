8447ba253da61278178d3ebea12feec6d6421aba
freek <freek@spatie.be>
2019-04-25 09:35:47 +0200

    only real tests should be public

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,27 +8,27 @@ use Spatie\Calendar\PropertyTypes\PropertyType;
 
 abstract class TestCase extends BaseTestCase
 {
-    public function assertInArray($needle, $haystack): void
+    protected function assertInArray($needle, $haystack): void
     {
         $this->assertContains($needle, $haystack, '', false, false);
     }
 
-    public function assertPropertyExistInPayload(string $name, ComponentPayload $componentPayload): void
+    protected function assertPropertyExistInPayload(string $name, ComponentPayload $componentPayload): void
     {
         $this->assertNotNull($componentPayload->getProperty($name));
     }
 
-    public function assertPropertyEqualsInPayload(string $name, $value, ComponentPayload $componentPayload): void
+    protected function assertPropertyEqualsInPayload(string $name, $value, ComponentPayload $componentPayload): void
     {
         $this->assertEquals($value, $componentPayload->getProperty($name)->getOriginalValue());
     }
 
-    public function assertAliasEqualsForProperty(string $propertyName, array $aliases, ComponentPayload $componentPayload): void
+    protected function assertAliasEqualsForProperty(string $propertyName, array $aliases, ComponentPayload $componentPayload): void
     {
         $this->assertEquals($aliases, $componentPayload->getAliasesForProperty($propertyName));
     }
 
-    public function assertParameterEqualsInProperty(string $name, $value, PropertyType $propertyType): void
+    protected function assertParameterEqualsInProperty(string $name, $value, PropertyType $propertyType): void
     {
         $this->assertEquals($value, $propertyType->getParameter($name)->getValue());
     }
