1b84045ff662e53bfc346491416f523df3af8a24
Alex Manase <malexandru699@gmail.com>
2022-10-25 15:21:02 +0300

    refactor `CoordinatesPropertyTest`

--- tests/Properties/CoordinatesPropertyTest.php
+++ tests/Properties/CoordinatesPropertyTest.php
@@ -1,21 +1,13 @@
 <?php
 
-namespace Spatie\IcalendarGenerator\Tests\Properties;
-
 use Spatie\IcalendarGenerator\Properties\CoordinatesProperty;
 use Spatie\IcalendarGenerator\Tests\PropertyExpectation;
-use Spatie\IcalendarGenerator\Tests\TestCase;
 
-class CoordinatesPropertyTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_a_coordinates_property_type()
-    {
-        $propertyType = new CoordinatesProperty('GEO', 10.5, 20.5);
+test('it can create a coordinates property type', function () {
+    $propertyType = new CoordinatesProperty('GEO', 10.5, 20.5);
 
-        PropertyExpectation::create($propertyType)
-            ->expectName('GEO')
-            ->expectOutput('10.5;20.5')
-            ->expectValue(['lat' => 10.5, 'lng' => 20.5]);
-    }
-}
+    PropertyExpectation::create($propertyType)
+        ->expectName('GEO')
+        ->expectOutput('10.5;20.5')
+        ->expectValue(['lat' => 10.5, 'lng' => 20.5]);
+});
