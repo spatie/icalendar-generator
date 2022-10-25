91059c58dafd2b2ef1d470d22fbb459bd19064a1
Alex Manase <malexandru699@gmail.com>
2022-10-25 15:32:47 +0300

    refactor `TestPropertyTest` and `UriPropertyTest`

--- tests/Properties/UriPropertyTest.php
+++ tests/Properties/UriPropertyTest.php
@@ -1,26 +1,16 @@
 <?php
 
-namespace Spatie\IcalendarGenerator\Tests\Properties;
-
 use Spatie\IcalendarGenerator\Properties\UriProperty;
 use Spatie\IcalendarGenerator\Tests\PropertyExpectation;
-use Spatie\IcalendarGenerator\Tests\TestCase;
 
-class UriPropertyTest extends TestCase
-{
-    /** @test */
-    public function it_accepts_an_uri()
-    {
-        PropertyExpectation::create(new UriProperty('', 'http://this.is/a/valid/uri'))
-            ->expectValue('http://this.is/a/valid/uri');
+test('it accepts an URI', function () {
+    PropertyExpectation::create(new UriProperty('', 'http://this.is/a/valid/uri'))
+        ->expectValue('http://this.is/a/valid/uri');
 
-        PropertyExpectation::create(new UriProperty('', 'foo://example.com:8042/over/there?name=ferret#nose'))
-            ->expectValue('foo://example.com:8042/over/there?name=ferret#nose');
-    }
+    PropertyExpectation::create(new UriProperty('', 'foo://example.com:8042/over/there?name=ferret#nose'))
+        ->expectValue('foo://example.com:8042/over/there?name=ferret#nose');
+});
 
-    /** @test */
-    public function it_will_return_an_empty_string_if_the_uri_is_not_valid()
-    {
-        PropertyExpectation::create(new UriProperty('', 'i-am-not-valid'))->expectOutput('');
-    }
-}
+test('it will return an empty string if the URI is not valid', function () {
+    PropertyExpectation::create(new UriProperty('', 'i-am-not-valid'))->expectOutput('');
+});
