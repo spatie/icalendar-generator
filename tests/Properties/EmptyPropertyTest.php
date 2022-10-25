b6f899d4c015a10002c377d9fd5dc6edf71e7acd
Alex Manase <malexandru699@gmail.com>
2022-10-25 15:26:48 +0300

    refactor `EmptyPropertyTest`

--- tests/Properties/EmptyPropertyTest.php
+++ tests/Properties/EmptyPropertyTest.php
@@ -1,25 +1,17 @@
 <?php
 
-namespace Spatie\IcalendarGenerator\Tests\Properties;
-
 use Spatie\IcalendarGenerator\Properties\EmptyProperty;
 use Spatie\IcalendarGenerator\Tests\PropertyExpectation;
-use Spatie\IcalendarGenerator\Tests\TestCase;
 
-class EmptyPropertyTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_an_empty_property_type()
-    {
-        $propertyType = new EmptyProperty('CONTACT', []);
+test('it can create an empty property type', function () {
+    $propertyType = new EmptyProperty('CONTACT', []);
 
-        $this->assertEquals('CONTACT', $propertyType->getName());
-        $this->assertEquals(null, $propertyType->getValue());
-        $this->assertEquals(null, $propertyType->getOriginalValue());
+    $this->assertEquals('CONTACT', $propertyType->getName());
+    $this->assertEquals(null, $propertyType->getValue());
+    $this->assertEquals(null, $propertyType->getOriginalValue());
 
-        PropertyExpectation::create($propertyType)
-            ->expectName('CONTACT')
-            ->expectValue(null)
-            ->expectOutput(null);
-    }
-}
+    PropertyExpectation::create($propertyType)
+        ->expectName('CONTACT')
+        ->expectValue(null)
+        ->expectOutput(null);
+});
