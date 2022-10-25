ad0889f11478165cb97f9751ed4b6c061a86a61f
Alex Manase <malexandru699@gmail.com>
2022-10-25 14:18:15 +0300

    refactor `TimezoneEntryTest`

--- tests/Components/TimezoneEntryTest.php
+++ tests/Components/TimezoneEntryTest.php
@@ -11,109 +11,96 @@ use Spatie\IcalendarGenerator\Tests\PayloadExpectation;
 use Spatie\IcalendarGenerator\Tests\TestCase;
 use Spatie\IcalendarGenerator\ValueObjects\RRule;
 
-class TimezoneEntryTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_a_standard_entry()
-    {
-        $payload = TimezoneEntry::create(
-            TimezoneEntryType::standard(),
-            new DateTime('16 may 2020 12:00:00'),
-            '+00:00',
-            '+02:00'
-        )->resolvePayload();
+use function Spatie\Snapshots\assertMatchesSnapshot;
 
-        PayloadExpectation::create($payload)
-            ->expectType('STANDARD')
-            ->expectPropertyValue('DTSTART', new DateTime('16 may 2020 12:00:00'))
-            ->expectPropertyValue('TZOFFSETFROM', '+00:00')
-            ->expectPropertyValue('TZOFFSETTO', '+02:00');
-    }
+test('it can create a standard entry', function () {
+    $payload = TimezoneEntry::create(
+        TimezoneEntryType::standard(),
+        new DateTime('16 may 2020 12:00:00'),
+        '+00:00',
+        '+02:00'
+    )->resolvePayload();
 
-    /** @test */
-    public function it_can_create_a_standard_entry_with_negative_offsets()
-    {
-        $payload = TimezoneEntry::create(
-            TimezoneEntryType::standard(),
-            new DateTime('16 may 2020 12:00:00'),
-            '-00:00',
-            '-02:00'
-        )->resolvePayload();
+    PayloadExpectation::create($payload)
+        ->expectType('STANDARD')
+        ->expectPropertyValue('DTSTART', new DateTime('16 may 2020 12:00:00'))
+        ->expectPropertyValue('TZOFFSETFROM', '+00:00')
+        ->expectPropertyValue('TZOFFSETTO', '+02:00');
+});
 
-        PayloadExpectation::create($payload)
-            ->expectType('STANDARD')
-            ->expectPropertyValue('DTSTART', new DateTime('16 may 2020 12:00:00'))
-            ->expectPropertyValue('TZOFFSETFROM', '-00:00')
-            ->expectPropertyValue('TZOFFSETTO', '-02:00');
-    }
+test('it can create a standard entry with negative offsets', function () {
+    $payload = TimezoneEntry::create(
+        TimezoneEntryType::standard(),
+        new DateTime('16 may 2020 12:00:00'),
+        '-00:00',
+        '-02:00'
+    )->resolvePayload();
 
-    /** @test */
-    public function it_can_create_a_daylight_entry()
-    {
-        $payload = TimezoneEntry::create(
-            TimezoneEntryType::daylight(),
-            new DateTime('16 may 2020 12:00:00'),
-            '+00:00',
-            '+02:00'
-        )->resolvePayload();
+    PayloadExpectation::create($payload)
+        ->expectType('STANDARD')
+        ->expectPropertyValue('DTSTART', new DateTime('16 may 2020 12:00:00'))
+        ->expectPropertyValue('TZOFFSETFROM', '-00:00')
+        ->expectPropertyValue('TZOFFSETTO', '-02:00');
+});
 
-        PayloadExpectation::create($payload)
-            ->expectType('DAYLIGHT')
-            ->expectPropertyValue('DTSTART', new DateTime('16 may 2020 12:00:00'))
-            ->expectPropertyValue('TZOFFSETFROM', '+00:00')
-            ->expectPropertyValue('TZOFFSETTO', '+02:00');
-    }
+test('it can create a daylight entry', function () {
+    $payload = TimezoneEntry::create(
+        TimezoneEntryType::daylight(),
+        new DateTime('16 may 2020 12:00:00'),
+        '+00:00',
+        '+02:00'
+    )->resolvePayload();
 
-    /** @test */
-    public function it_can_set_a_name_and_description()
-    {
-        $payload = TimezoneEntry::create(
-            TimezoneEntryType::standard(),
-            new DateTime('16 may 2020 12:00:00'),
-            '+00:00',
-            '+02:00'
-        )
-            ->name('Europe - Brussels')
-            ->description('Belgian timezones ftw!')
-            ->resolvePayload();
+    PayloadExpectation::create($payload)
+        ->expectType('DAYLIGHT')
+        ->expectPropertyValue('DTSTART', new DateTime('16 may 2020 12:00:00'))
+        ->expectPropertyValue('TZOFFSETFROM', '+00:00')
+        ->expectPropertyValue('TZOFFSETTO', '+02:00');
+});
 
-        PayloadExpectation::create($payload)
-            ->expectPropertyValue('TZNAME', 'Europe - Brussels')
-            ->expectPropertyValue('COMMENT', 'Belgian timezones ftw!');
-    }
+test('it can set a name and description', function () {
+    $payload = TimezoneEntry::create(
+        TimezoneEntryType::standard(),
+        new DateTime('16 may 2020 12:00:00'),
+        '+00:00',
+        '+02:00'
+    )
+        ->name('Europe - Brussels')
+        ->description('Belgian timezones ftw!')
+        ->resolvePayload();
 
-    /** @test */
-    public function it_can_set_a_rrule()
-    {
-        $payload = TimezoneEntry::create(
-            TimezoneEntryType::standard(),
-            new DateTime('16 may 2020 12:00:00'),
-            '+00:00',
-            '+02:00'
-        )
-            ->rrule(RRule::frequency(RecurrenceFrequency::daily()))
-            ->resolvePayload();
+    PayloadExpectation::create($payload)
+        ->expectPropertyValue('TZNAME', 'Europe - Brussels')
+        ->expectPropertyValue('COMMENT', 'Belgian timezones ftw!');
+});
 
-        PayloadExpectation::create($payload)
-            ->expectPropertyValue('RRULE', RRule::frequency(RecurrenceFrequency::daily()));
-    }
+test('it can set a rrule', function () {
+    $payload = TimezoneEntry::create(
+        TimezoneEntryType::standard(),
+        new DateTime('16 may 2020 12:00:00'),
+        '+00:00',
+        '+02:00'
+    )
+        ->rrule(RRule::frequency(RecurrenceFrequency::daily()))
+        ->resolvePayload();
 
-    /** @test */
-    public function it_can_write_out_a_timezone_entry()
-    {
-        $payload = TimezoneEntry::create(
-            TimezoneEntryType::daylight(),
-            new DateTime('16 may 2020 12:00:00'),
-            '+00:00',
-            '+02:00'
-        )
-            ->rrule(RRule::frequency(RecurrenceFrequency::daily()))
-            ->name('Europe - Brussels')
-            ->description('Belgian timezones ftw!')
-            ->resolvePayload();
+    PayloadExpectation::create($payload)
+        ->expectPropertyValue('RRULE', RRule::frequency(RecurrenceFrequency::daily()));
+});
 
-        $written = ComponentBuilder::create($payload)->build();
+test('it can write out a timezone entry', function () {
+    $payload = TimezoneEntry::create(
+        TimezoneEntryType::daylight(),
+        new DateTime('16 may 2020 12:00:00'),
+        '+00:00',
+        '+02:00'
+    )
+        ->rrule(RRule::frequency(RecurrenceFrequency::daily()))
+        ->name('Europe - Brussels')
+        ->description('Belgian timezones ftw!')
+        ->resolvePayload();
 
-        $this->assertMatchesSnapshot($written);
-    }
-}
+    $written = ComponentBuilder::create($payload)->build();
+
+    assertMatchesSnapshot($written);
+});
