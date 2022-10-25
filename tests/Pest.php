292ebf5efcf4d5343623e4f2b18b8abdb016441f
Alex Manase <malexandru699@gmail.com>
2022-10-25 14:21:10 +0300

    refactor `TimezoneTest`

--- tests/Pest.php
+++ tests/Pest.php
@@ -6,6 +6,9 @@
 |--------------------------------------------------------------------------
 */
 
+use Spatie\IcalendarGenerator\Components\TimezoneEntry;
+use Spatie\IcalendarGenerator\Enums\TimezoneEntryType;
+
 uses(\Spatie\IcalendarGenerator\Tests\TestCase::class)->in('.');
 
 /*
@@ -19,3 +22,13 @@ uses(\Spatie\IcalendarGenerator\Tests\TestCase::class)->in('.');
 | Functions
 |--------------------------------------------------------------------------
 */
+
+function createTimezoneEntry(): TimezoneEntry
+{
+    return TimezoneEntry::create(
+        TimezoneEntryType::standard(),
+        new DateTime('16 may 2020 12:00:00'),
+        '+00:00',
+        '+02:00'
+    );
+}
