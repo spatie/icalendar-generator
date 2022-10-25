5046e65741a4cb728566f580a1e8ec33d3fbd84a
Alex Manase <malexandru699@gmail.com>
2022-10-25 15:25:25 +0300

    refactor `DateTimePropertyTest`

--- tests/Properties/DateTimePropertyTest.php
+++ tests/Properties/DateTimePropertyTest.php
@@ -1,110 +1,81 @@
 <?php
 
-namespace Spatie\IcalendarGenerator\Tests\Properties;
-
-use DateTime;
-use DateTimeZone;
 use Spatie\IcalendarGenerator\Properties\DateTimeProperty;
 use Spatie\IcalendarGenerator\Tests\PropertyExpectation;
-use Spatie\IcalendarGenerator\Tests\TestCase;
 use Spatie\IcalendarGenerator\ValueObjects\DateTimeValue;
 
-class DateTimePropertyTest extends TestCase
-{
-    protected DateTime $date;
+beforeEach(function () {
+    $this->date = (new DateTime('16 may 2019 12:10:15', new DateTimeZone('Europe/Brussels')));
+});
 
-    protected function setUp() : void
-    {
-        parent::setUp();
+test('it will format the date correctly', function () {
+    $property = DateTimeProperty::fromDateTime('STARTS', $this->date);
 
-        $this->date = (new DateTime('16 may 2019 12:10:15', new DateTimeZone('Europe/Brussels')));
-    }
+    PropertyExpectation::create($property)->expectOutput('20190516');
+});
 
-    /** @test */
-    public function it_will_format_the_date_correctly()
-    {
-        $property = DateTimeProperty::fromDateTime('STARTS', $this->date);
+test('it will format the date and time correctly with timezone', function () {
+    $property = DateTimeProperty::fromDateTime('STARTS', $this->date, true);
 
-        PropertyExpectation::create($property)->expectOutput('20190516');
-    }
+    PropertyExpectation::create($property)
+        ->expectOutput('20190516T121015')
+        ->expectParameterCount(1)
+        ->expectParameterValue('TZID', 'Europe/Brussels');
+});
 
-    /** @test */
-    public function it_will_format_the_date_and_time_correctly_with_timezone()
-    {
-        $property = DateTimeProperty::fromDateTime('STARTS', $this->date, true);
+test('it will use a specific UTC format', function () {
+    $this->date->setTimezone(new DateTimeZone('UTC'));
 
-        PropertyExpectation::create($property)
-            ->expectOutput('20190516T121015')
-            ->expectParameterCount(1)
-            ->expectParameterValue('TZID', 'Europe/Brussels');
-    }
+    $property = DateTimeProperty::fromDateTime('STARTS', $this->date, true);
 
-    /** @test */
-    public function it_will_use_a_specific_utc_format()
-    {
-        $this->date->setTimezone(new DateTimeZone('UTC'));
+    PropertyExpectation::create($property)
+        ->expectOutput('20190516T101015Z')
+        ->expectParameterCount(0);
+});
 
-        $property = DateTimeProperty::fromDateTime('STARTS', $this->date, true);
+test('it will not use a specific UTC format when time is not given', function () {
+    $this->date->setTimezone(new DateTimeZone('UTC'));
 
-        PropertyExpectation::create($property)
-            ->expectOutput('20190516T101015Z')
-            ->expectParameterCount(0);
-    }
+    $property = DateTimeProperty::fromDateTime('STARTS', $this->date);
 
-    /** @test */
-    public function it_will_not_use_a_specific_utc_format_when_time_is_not_given()
-    {
-        $this->date->setTimezone(new DateTimeZone('UTC'));
+    PropertyExpectation::create($property)
+        ->expectOutput('20190516')
+        ->expectParameterCount(1)
+        ->expectParameterValue('VALUE', 'DATE');
+});
 
-        $property = DateTimeProperty::fromDateTime('STARTS', $this->date);
+test('it will use a non-UTC timezone format when time is not given', function () {
+    $property = DateTimeProperty::fromDateTime('STARTS', $this->date, false);
 
-        PropertyExpectation::create($property)
-            ->expectOutput('20190516')
-            ->expectParameterCount(1)
-            ->expectParameterValue('VALUE', 'DATE');
-    }
+    PropertyExpectation::create($property)
+        ->expectOutput('20190516')
+        ->expectParameterCount(2)
+        ->expectParameterValue('TZID', 'Europe/Brussels')
+        ->expectParameterValue('VALUE', 'DATE');
+});
 
-    /** @test */
-    public function it_will_use_a_non_utc_timezone_format_when_time_is_not_given()
-    {
-        $property = DateTimeProperty::fromDateTime('STARTS', $this->date, false);
+test('it will format the date and time correctly with a conversion to another timezone', function () {
+    $this->date->setTimezone(new DateTimeZone('Europe/Brussels'));
 
-        PropertyExpectation::create($property)
-            ->expectOutput('20190516')
-            ->expectParameterCount(2)
-            ->expectParameterValue('TZID', 'Europe/Brussels')
-            ->expectParameterValue('VALUE', 'DATE');
-    }
+    $property = DateTimeProperty::fromDateTime('STARTS', $this->date, true);
 
-    /** @test */
-    public function it_will_format_the_date_and_time_correctly_with_a_conversion_to_another_timezone()
-    {
-        $this->date->setTimezone(new DateTimeZone('Europe/Brussels'));
+    PropertyExpectation::create($property)
+        ->expectOutput('20190516T121015')
+        ->expectParameterCount(1)
+        ->expectParameterValue('TZID', 'Europe/Brussels');
+});
 
-        $property = DateTimeProperty::fromDateTime('STARTS', $this->date, true);
+test('it will format the date and time without timezone', function () {
+    $property = DateTimeProperty::fromDateTime('STARTS', $this->date, true, true);
 
-        PropertyExpectation::create($property)
-            ->expectOutput('20190516T121015')
-            ->expectParameterCount(1)
-            ->expectParameterValue('TZID', 'Europe/Brussels');
-    }
+    PropertyExpectation::create($property)->expectOutput('20190516T121015');
+});
 
-    /** @test */
-    public function it_will_format_the_date_and_time_without_timezone()
-    {
-        $property = DateTimeProperty::fromDateTime('STARTS', $this->date, true, true);
+test('it can be created from a DateTime value', function () {
+    $property = DateTimeProperty::create(
+        'STARTS',
+        DateTimeValue::create($this->date)
+    );
 
-        PropertyExpectation::create($property)->expectOutput('20190516T121015');
-    }
-
-    /** @test */
-    public function it_can_be_created_from_a_date_time_value()
-    {
-        $property = DateTimeProperty::create(
-            'STARTS',
-            DateTimeValue::create($this->date)
-        );
-
-        PropertyExpectation::create($property)->expectOutput('20190516T121015');
-    }
-}
+    PropertyExpectation::create($property)->expectOutput('20190516T121015');
+});
