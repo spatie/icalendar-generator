57fc7f94d19ec4d49f93b5461863638eceb7bf85
Alex Manase <malexandru699@gmail.com>
2022-10-25 15:30:07 +0300

    refactor `RRulePropertyTest`

--- tests/Properties/RRulePropertyTest.php
+++ tests/Properties/RRulePropertyTest.php
@@ -1,25 +1,17 @@
 <?php
 
-namespace Spatie\IcalendarGenerator\Tests\Properties;
-
 use Spatie\IcalendarGenerator\Enums\RecurrenceFrequency;
 use Spatie\IcalendarGenerator\Properties\RRuleProperty;
 use Spatie\IcalendarGenerator\Tests\PropertyExpectation;
-use Spatie\IcalendarGenerator\Tests\TestCase;
 use Spatie\IcalendarGenerator\ValueObjects\RRule;
 
-class RRulePropertyTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_a_recurrence_rule_property_type()
-    {
-        $recurrenceRule = RRule::frequency(RecurrenceFrequency::daily());
+test('it can create a recurrence rule property type', function () {
+    $recurrenceRule = RRule::frequency(RecurrenceFrequency::daily());
 
-        $propertyType = RRuleProperty::create('RRULE', $recurrenceRule);
+    $propertyType = RRuleProperty::create('RRULE', $recurrenceRule);
 
-        PropertyExpectation::create($propertyType)
-            ->expectName('RRULE')
-            ->expectOutput('FREQ=DAILY')
-            ->expectValue($recurrenceRule);
-    }
-}
+    PropertyExpectation::create($propertyType)
+        ->expectName('RRULE')
+        ->expectOutput('FREQ=DAILY')
+        ->expectValue($recurrenceRule);
+});
