spatie/icalendar-generator

commit b082f423647c149d95a69ac2fc0fbe562897b29e

Allow adding rrules as string

src/Components/Event.php
@@ -70,7 +70,12 @@ class Event extends Component implements HasTimezones
 
     private ?EventStatus $status = null;
 
-    private ?RRule $rrule = null;
+    /** @var RRule|string|null */
+    private $rrule = null;
+
+    private ?DateTime $rruleStarting = null;
+
+    private ?DateTime $rruleUntil = null;
 
     /** @var \Spatie\IcalendarGenerator\ValueObjects\DateTimeValue[] */
     private array $recurrence_dates = [];
@@ -295,6 +300,15 @@ class Event extends Component implements HasTimezones
         return $this;
     }
 
+    public function rruleAsString(string $rrule, ?DateTimeInterface $starting = null, DateTimeInterface $until = null): Event
+    {
+        $this->rrule = $rrule;
+        $this->rruleStarting = $starting;
+        $this->rruleUntil = $until;
+
+        return $this;
+    }
+
     /**
      * @param DateTimeInterface[]|DateTimeInterface $dates
      * @param bool $withTime
@@ -379,7 +393,10 @@ class Event extends Component implements HasTimezones
             ->add($this->starts)
             ->add($this->ends)
             ->add($this->created)
-            ->add($this->rrule)
+            ->add(is_string($this->rrule)
+                ? [$this->rruleStarting, $this->rruleUntil]
+                : $this->rrule
+            )
             ->add($this->recurrence_dates)
             ->add($this->excluded_recurrence_dates);
     }
@@ -441,7 +458,9 @@ class Event extends Component implements HasTimezones
             )
             ->optional(
                 $this->rrule,
-                fn () => RRuleProperty::create('RRULE', $this->rrule)
+                fn () => is_string($this->rrule)
+                    ? TextProperty::create('RRULE', $this->rrule)
+                    : RRuleProperty::create('RRULE', $this->rrule)
             )
             ->multiple(
                 $this->attendees,

tests/Components/EventTest.php
@@ -258,6 +258,15 @@ test('it an set a recurrence rule', function () {
         ->expectValue($rrule);
 });
 
+test('it an set a recurrence rule as a string', function () {
+    $payload = Event::create('An introduction into event sourcing')
+        ->rruleAsString($rrule = 'FREQ=DAILY;INTERVAL=2;UNTIL=20240301T230000Z')
+        ->resolvePayload();
+
+    PropertyExpectation::create($payload, 'RRULE')
+        ->expectValue($rrule);
+});
+
 test('it can create an event without timezones', function () {
     $dateAlert = new DateTime('17 may 2019 11:00:00');
     $dateStarts = new DateTime('17 may 2019 12:00:00');

src/Properties/RRuleProperty.php
@@ -32,6 +32,6 @@ class RRuleProperty extends Property
 
     public function getOriginalValue(): RRule
     {
-        return$this->recurrenceRule;
+        return $this->recurrenceRule;
     }
 }

commit f895747b71a41994b0d85ebdace2712a53fb9cfc

Fix 2 bugs introduced in https://github.com/michaelKaefer/icalendar-generator/commit/b082f423647c149d95a69ac2fc0fbe562897b29e

src/Components/Event.php
@@ -73,9 +73,9 @@ class Event extends Component implements HasTimezones
     /** @var RRule|string|null */
     private $rrule = null;
 
-    private ?DateTime $rruleStarting = null;
+    private ?DateTimeInterface $rruleStarting = null;
 
-    private ?DateTime $rruleUntil = null;
+    private ?DateTimeInterface $rruleUntil = null;
 
     /** @var \Spatie\IcalendarGenerator\ValueObjects\DateTimeValue[] */
     private array $recurrence_dates = [];
@@ -460,7 +460,7 @@ class Event extends Component implements HasTimezones
             ->optional(
                 $this->rrule,
                 fn () => is_string($this->rrule)
-                    ? TextProperty::create('RRULE', $this->rrule)
+                    ? TextProperty::create('RRULE', $this->rrule)->withoutEscaping()
                     : RRuleProperty::create('RRULE', $this->rrule)
             )
             ->multiple(

tests/Components/EventTest.php
@@ -249,7 +249,7 @@ test('it can set an address without name', function () {
         ->expectValue('Antwerp');
 });
 
-test('it an set a recurrence rule', function () {
+test('it can set a recurrence rule', function () {
     $payload = Event::create('An introduction into event sourcing')
         ->rrule($rrule = RRule::frequency(RecurrenceFrequency::daily()))
         ->resolvePayload();
@@ -258,13 +258,14 @@ test('it an set a recurrence rule', function () {
         ->expectValue($rrule);
 });
 
-test('it an set a recurrence rule as a string', function () {
-    $payload = Event::create('An introduction into event sourcing')
+test('it can set a recurrence rule as a string', function () {
+    $payload = Event::create('A recurring event')
         ->rruleAsString($rrule = 'FREQ=DAILY;INTERVAL=2;UNTIL=20240301T230000Z')
         ->resolvePayload();
 
     PropertyExpectation::create($payload, 'RRULE')
-        ->expectValue($rrule);
+        ->expectValue($rrule)
+        ->expectOutput($rrule);
 });
 
 test('it can create an event without timezones', function () {