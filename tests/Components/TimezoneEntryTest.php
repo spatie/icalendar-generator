47d04ae3ca38061d527c317bc092e6ae718843fc
atymic <atymicq@gmail.com>
2025-11-18 14:58:13 +1100

    fix: full timezone offset issue half hour

--- tests/Components/TimezoneEntryTest.php
+++ tests/Components/TimezoneEntryTest.php
@@ -100,3 +100,73 @@ test('it can write out a timezone entry', function () {
 
     assertMatchesSnapshot($written);
 });
+
+test('it correctly formats half-hour offsets when created from transition', function () {
+    $positiveOffsetFrom = new DateInterval('PT9H30M');
+    $positiveOffsetFrom->invert = 0;
+
+    $positiveOffsetTo = new DateInterval('PT10H30M');
+    $positiveOffsetTo->invert = 0;
+
+    $positiveTransition = new \Spatie\IcalendarGenerator\Timezones\TimezoneTransition(
+        new DateTime('2025-10-05 02:00:00'),
+        $positiveOffsetFrom,
+        $positiveOffsetTo,
+        TimezoneEntryType::Daylight
+    );
+
+    $positiveEntry = TimezoneEntry::createFromTransition($positiveTransition);
+    $positivePayload = $positiveEntry->resolvePayload();
+
+    PayloadExpectation::create($positivePayload)
+        ->expectType('DAYLIGHT')
+        ->expectPropertyValue('TZOFFSETFROM', '+0930')
+        ->expectPropertyValue('TZOFFSETTO', '+1030');
+
+    $negativeOffsetFrom = new DateInterval('PT3H30M');
+    $negativeOffsetFrom->invert = 1;
+
+    $negativeOffsetTo = new DateInterval('PT4H30M');
+    $negativeOffsetTo->invert = 1;
+
+    $negativeTransition = new \Spatie\IcalendarGenerator\Timezones\TimezoneTransition(
+        new DateTime('2025-04-06 03:00:00'),
+        $negativeOffsetFrom,
+        $negativeOffsetTo,
+        TimezoneEntryType::Standard
+    );
+
+    $negativeEntry = TimezoneEntry::createFromTransition($negativeTransition);
+    $negativePayload = $negativeEntry->resolvePayload();
+
+    PayloadExpectation::create($negativePayload)
+        ->expectType('STANDARD')
+        ->expectPropertyValue('TZOFFSETFROM', '-0330')
+        ->expectPropertyValue('TZOFFSETTO', '-0430');
+});
+
+test('it correctly handles Australia Adelaide timezone with half-hour offsets', function () {
+    $resolver = new \Spatie\IcalendarGenerator\Timezones\TimezoneTransitionsResolver(
+        new DateTimeZone('Australia/Adelaide'),
+        new DateTime('2025-08-01 00:00:00'),
+        new DateTime('2025-08-31 23:59:59')
+    );
+
+    $transitions = $resolver->getTransitions();
+
+    $standardEntry = TimezoneEntry::createFromTransition($transitions[0]);
+    $standardPayload = $standardEntry->resolvePayload();
+
+    PayloadExpectation::create($standardPayload)
+        ->expectType('STANDARD')
+        ->expectPropertyValue('TZOFFSETFROM', '+1030')
+        ->expectPropertyValue('TZOFFSETTO', '+0930');
+
+    $daylightEntry = TimezoneEntry::createFromTransition($transitions[1]);
+    $daylightPayload = $daylightEntry->resolvePayload();
+
+    PayloadExpectation::create($daylightPayload)
+        ->expectType('DAYLIGHT')
+        ->expectPropertyValue('TZOFFSETFROM', '+0930')
+        ->expectPropertyValue('TZOFFSETTO', '+1030');
+});
